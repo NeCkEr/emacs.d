7d9d83f895b6a365590cd6ad7ac8607d8b20815c
NeCkEr <mail@brunex.name>
2018-08-11 17:11:13 +0100

    first commit fingers crossed :)

---
+++ init.el
@@ -0,0 +1,14 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+;; keep the installed packages in .emacs.d
+(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+(package-initialize)
+
+;; update the package metadata is the local cache is missing
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
